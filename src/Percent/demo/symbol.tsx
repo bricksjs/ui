41627e9fa479ad74e0c5f4d30ab712ba0074cc71
聂海锋 <niexiaofei@yahoo.com>
2020-11-24 01:51:46 +0800

    feat: 修复错误

--- src/Percent/demo/symbol.tsx
+++ src/Percent/demo/symbol.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import { Percent } from '@bricksjs/react-ui';
 
 export default () => {
-  return <Percent value={10 ** 8} precision={2} symbol={{ cent: 'pt' }} />;
+  return (
+    <Percent
+      value={10 ** 8}
+      precision={2}
+      symbol={{ cent: 'pt', calc: true }}
+    />
+  );
 };
